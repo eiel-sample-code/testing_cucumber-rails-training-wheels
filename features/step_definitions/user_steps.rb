226e24211f518d14dae31cb1c5ef76622d0ef704
Tomohiko Himura <eiel.hal@gmail.com>
2012-10-05 11:06:56 +0900

    tableish してみた

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -10,5 +10,7 @@ When /^I delete the (\d+)(?:st|nd|rd|th) user$/ do |pos|
 end
 
 Then /^I should see the following users:$/ do |expected_users_table|
-  expected_users_table.diff!(tableish('table tr', 'td,th'))
+  rows = find("table").all('tr')
+  table = rows.map { |r| r.all("td,th").map { |c| c.text.strip } }
+  expected_users_table.diff!(table)
 end
